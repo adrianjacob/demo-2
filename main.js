d8b4ffe7bf8c446a17bb85095c0fdbb85b068e62
Benedict Hartley <spam@chaffin.ch>
2015-01-30 11:45:36 +0000

    Moved line chart logic into main.js

--- main.js
+++ main.js
@@ -104,40 +104,112 @@
 				color: '#d2234f'
 			}]
 		}]
+	},
+
+	line: {
+		chart: {
+			renderTo: 'line',
+			type: 'spline',
+			margin: 0,
+			marginBottom: 50
+		},
+		title: {
+			text: null
+		},
+		credits: {
+			enabled: false
+		},
+		legend: {
+			enabled: false
+		},
+		xAxis: {
+			tickmarkPlacement: 'on',
+			tickWidth: 0,
+			gridLineWidth: 1,
+			categories: ['28 Jan', '05 Feb', '12 Feb', '19 Feb', '26 Feb', '02 Mar', '09 Mar', '16 Mar', '23 Mar', '30 Mar']
+		},
+		yAxis: {
+			title: {
+				text: null
+
+			},
+			labels: {
+				align:'left',
+				x:0,
+				y:-2,
+				style: {
+					'color':'#CCC'
+				},
+				formatter: function () {
+					return '£' + this.value;
+				}
+			}
+		},
+		tooltip: {
+			pointFormat: '{series.name} = <b>£{point.y:,.0f}</b>'
+		},
+		plotOptions: {
+			spline: {
+				marker: {
+					symbol: 'circle'
+				},
+				animation: false
+			}
+
+		},
+		series: [{
+			color: '#999',
+			name: 'Total',
+			data: random(650)
+		},{
+			color: '#9cbf2b',
+			name: 'Payment',
+			data: random(500)
+		}, {
+			color: '#d2234f',
+			name: 'Deduction',
+			data: random(100)
+		}]
 	}
 
 };
 
  $(function() {
 
-	var currentChart;
+	var currentCharts = [];
 	var position = 1;
 	var spanleft;
 	var spanwidth = $('.nav a.active').outerWidth();
 
-	$('.nav span').css('width',spanwidth)
-	$('.nav span').css('left',$('.nav a.active').offset().left + $('.nav').scrollLeft())
+	$('.nav span').css('width',spanwidth);
+	$('.nav span').css('left',$('.nav a.active').offset().left + $('.nav').scrollLeft());
 
-	function injectChart(position) {
+	function injectCharts(position) {
+		chartData.line.xAxis.tickPositions = [position];
 		charticide();
-		currentChart = new Highcharts.Chart(chartData['chart' + position]);
-		return currentChart;
+		currentCharts = [
+			new Highcharts.Chart(chartData['chart' + position]),
+			new Highcharts.Chart(chartData.line)
+		];
+		return currentCharts;
 	}
 
 	function charticide() {
-		return (currentChart) ? currentChart.destroy() : null;
+		return currentCharts.map(function(chart) {
+			return chart.destroy();
+		});
 	}
 
 	function viewportWidth() {
 		return Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
 	}
 
-	injectChart(position);
+	injectCharts(position);
 
 	$('.charts').hammer().on("swiperight", function(ev) {
 		//if(position<2) return;
 		position += 1;
-		injectChart(position);
+		injectCharts(position);
 	    $( ".charts ul" ).css('left','+='+viewportWidth()+'');
 	    $('.nav a.active').removeClass('active').prev().addClass('active');
 	    spanleft = $('.nav a.active').offset().left;
@@ -147,11 +219,11 @@
 	 $('.charts').hammer().on("swipeleft", function(e) {
 	 	if(position<2) return;
 		position -= 1;
-		injectChart(position);
+		injectCharts(position);
 	    $( ".charts ul" ).css('left','-='+viewportWidth()+'');
 	    $('.nav a.active').removeClass().next().addClass('active');
 	    spanleft = $('.nav a.active').offset().left + $('.nav').scrollLeft();
-	    $('.nav span').css('left',spanleft)
+	    $('.nav span').css('left',spanleft);
 	});
 
 	$('.tabletoggle').hammer().on("tap", function(e) {
@@ -159,3 +231,14 @@
 	});
 
 });
+
+function random(max) {
+	var items = 10;
+	var arr = [];
+	for(var i = 0; i<items; i++) {
+		var bar = Math.floor(Math.random() * 50) + max;
+		arr.push(bar);
+	}
+	return arr;
+}
+
